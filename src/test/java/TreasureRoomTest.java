a23284f5fe4155121aa50d2e6ea993b9ee7c69a9
Stephen <stephen_h369@live.co.uk>
2020-09-02 16:04:34 +0100

    can add and remove players from treasure room

--- src/test/java/TreasureRoomTest.java
+++ src/test/java/TreasureRoomTest.java
@@ -1,7 +1,10 @@
 import components.Treasure;
 import components.TreasureValue;
+import components.Weapon;
 import org.junit.Before;
 import org.junit.Test;
+import players.Player;
+import players.melee.Knight;
 import rooms.TreasureRoom;
 
 import java.util.ArrayList;
@@ -10,12 +13,16 @@ import static org.junit.Assert.assertEquals;
 
 public class TreasureRoomTest {
 
+    Weapon equippedWeapon2;
+    Player player1;
     TreasureRoom treasureRoom;
     Treasure treasure;
     ArrayList<Treasure> treasures;
 
     @Before
     public void before() {
+        equippedWeapon2 = new Weapon("Sword", 5);
+        player1 = new Knight(100, 10, equippedWeapon2);
         treasure = new Treasure("Gold", TreasureValue.RARE);
         treasures = new ArrayList<Treasure>();
         treasures.add(treasure);
@@ -29,6 +36,19 @@ public class TreasureRoomTest {
 
     @Test
     public void canGetPlayerCount() {
+        assertEquals(0, treasureRoom.getPlayerCount());
+    }
+
+    @Test
+    public void canAddPlayer() {
+        treasureRoom.addPlayer(player1);
+        assertEquals(1, treasureRoom.getPlayerCount());
+    }
+
+    @Test
+    public void canRemovePlayer() {
+        treasureRoom.addPlayer(player1);
+        treasureRoom.removePlayer(player1);
         assertEquals(0, treasureRoom.getPlayerCount());
     }
 }
