c6fa640afff277c7ef270696cffd1ca4ccfce530
Stephen <stephen_h369@live.co.uk>
2020-09-02 14:25:01 +0100

    wizard can return treasure count, add and remove treasure & health

--- src/test/java/WizardTest.java
+++ src/test/java/WizardTest.java
@@ -35,4 +35,34 @@ public class WizardTest {
     public void canGetCompanion() {
         assertEquals(companion, wizard.getCompanion());
     }
+
+    @Test
+    public void canGetTreasureCount() {
+        assertEquals(0, wizard.getTreasureCount());
+    }
+
+    @Test
+    public void canAddTreasure() {
+        wizard.addTreasure(treasure);
+        assertEquals(1, wizard.getTreasureCount());
+    }
+
+    @Test
+    public void canRemoveTreasure() {
+        wizard.addTreasure(treasure);
+        wizard.removeTreasure(treasure);
+        assertEquals(0, wizard.getTreasureCount());
+    }
+
+    @Test
+    public void canAddHealth() {
+        wizard.addHealth(10);
+        assertEquals(110, wizard.getHealth());
+    }
+
+    @Test
+    public void canRemoveHealth() {
+        wizard.removeHealth(10);
+        assertEquals(90, wizard.getHealth());
+    }
 }
